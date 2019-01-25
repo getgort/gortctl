a22af9387a33143225815e5656d7e7c97a408154
Matthew Titmus <matthew.titmus@gmail.com>
2019-01-25 13:04:16 -0500

    v0.3.0-dev.2 Bootstrap improvements; can no longer delete admin

--- cmd/bootstrap.go
+++ cmd/bootstrap.go
@@ -10,18 +10,37 @@ import (
 
 const (
 	bootstrapUse   = "bootstrap"
-	bootstrapShort = "Bootstrap a Cog serverr"
-	bootstrapLong  = `Bootstrap can be used on a brand new server to create the administrative user (admin).
-	
-It will only work on an instance that doesn't yet have any users defined.`
+	bootstrapShort = "Bootstrap a Cog server"
+	bootstrapLong  = `Bootstrap can be used on a brand new server, addressable at the specified URL,
+to create the administrative user ("admin"). Bootstrapping will only work on
+an instance that doesn't yet have any users defined.
+
+Following a successful bootstrapping, the returned password and user
+information are added to cogctl's configuration file as a new profile. If this
+is the first profile to be added to this configuration file, it will be marked
+as the default.
+
+By default, the new profile will be named for the hostname of the server being
+bootstrapped. This can be overridden using the -P or --profile flags.`
+
+	bootstrapUsage = `Usage:
+  cogctl bootstrap [flags] [URL]
+
+Flags:
+  -e, --email string      Email for the bootstrapped user (default "admin@cog")
+  -h, --help              help for bootstrap
+  -n, --name string       Full name of the bootstrapped user (default "Cog Administrator")
+  -p, --password string   Password for the bootstrapped user (default generated)
+
+Global Flags:
+  -P, --profile string   Cog profile to use
+`
 )
 
 var (
 	flagBootstrapEmail    string
 	flagBootstrapName     string
 	flagBootstrapPassword string
-	flagBootstrapProfile  string
-	flagBootstrapUser     string
 )
 
 // GetBootstrapCmd bootstrap
@@ -36,15 +55,16 @@ func GetBootstrapCmd() *cobra.Command {
 
 	cmd.Flags().StringVarP(&flagBootstrapEmail, "email", "e", "admin@cog", "Email for the bootstrapped user")
 	cmd.Flags().StringVarP(&flagBootstrapName, "name", "n", "Cog Administrator", "Full name of the bootstrapped user")
-	cmd.Flags().StringVarP(&flagBootstrapPassword, "password", "p", "", "Password for bootstrapped user")
-	cmd.Flags().StringVarP(&flagBootstrapUser, "username", "u", "admin", "Username for the bootstrapped user")
+	cmd.Flags().StringVarP(&flagBootstrapPassword, "password", "p", "", "Password for bootstrapped user (default generated)")
+
+	cmd.SetUsageTemplate(bootstrapUsage)
 
 	return cmd
 }
 
 func bootstrapCmd(cmd *cobra.Command, args []string) error {
 	entry := client.ProfileEntry{
-		Name:      flagBootstrapProfile,
+		Name:      FlagCogProfile,
 		URLString: args[0],
 	}
 
@@ -57,7 +77,6 @@ func bootstrapCmd(cmd *cobra.Command, args []string) error {
 		Email:    flagBootstrapEmail,
 		FullName: flagBootstrapName,
 		Password: flagBootstrapPassword,
-		Username: flagBootstrapUser,
 	}
 
 	// Client Bootstrap will create the cog config if necessary, and append
--- version.go
+++ version.go
@@ -2,5 +2,5 @@ package main
 
 const (
 	// Version is the current version of cogctl.
-	Version = "0.3.0-dev.1"
+	Version = "0.3.0-dev.2"
 )
